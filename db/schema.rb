b97388d366749ae7a76deb0e57794f94020fb80b
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-06-01 18:14:56 +0300

    Add column for post id

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_134437) do
+ActiveRecord::Schema.define(version: 2021_06_01_150714) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
